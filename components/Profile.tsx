9f8960d7301fe39d2673d08787a13e7fa5f48458
Asad Jawaid <asazycat@gmail.com>
2024-02-26 12:08:32 +0000

    images

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -23,12 +23,12 @@ export default function Profile () {
         <div className="logoIcons">
          <div className="gridItem">
             <a href="https://github.com/asazycat">
-        <img src="../src/imgs/github-icon.jpg" alt="" width="100" height="100" className="gitHubIcon"/>
+        <img src="../images/github-icon.jpg" alt="" width="100" height="100" className="gitHubIcon"/>
             </a>
         </div>   
         <div className="gridItem">
         <a href="https://www.linkedin.com/in/asad-jawaid-0022a016b/">
-        <img src="../src/imgs/linkedIn.jpg" alt="" width="100" height="100" className="linkedIn"/>
+        <img src="../images/linkedIn.jpg" alt="" width="100" height="100" className="linkedIn"/>
         </a>
         </div>
         </div>
