8951ad6d7b700f05a7f6971a6ba03dd3ee75e73f
Asad Jawaid <asazycat@gmail.com>
2024-02-26 15:43:57 +0000

    hosting

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <div className="Contact">
-            <h1 id="contact">Contact (<a href="/src/Data/cv.doc" download="w3logo">Download CV</a>)</h1>
+            <h1 id="contact">Contact (<a href="/src/Data/AsadJawaidCV.pdf" download="w3logo">Download CV</a>)</h1>
             <p>Email: asazycat@gmail.com</p>
             <p>Number: +44 7473201525</p>
         </div>
