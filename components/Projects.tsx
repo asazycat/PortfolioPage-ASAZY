8136888ef45290acf08a0e1788a8c11fd7ef1a2b
Asad Jawaid <asazycat@gmail.com>
2024-02-26 01:52:15 +0000

    portfolio overhaul

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,7 +9,7 @@ export default function Projects () {
                 <h2>Norcthcoders News Network (NC News) - Solo</h2>
                 <div className="desImgFormat">
                 <p>Project description</p>
-                <img src="../src/imgs/articlesNCNews.png" alt="" width="800" height="600"/>
+                <img src="../src/imgs/articlesNCNews.jpg" alt="" width="800" height="600"/>
                 </div>
                 <div className="links">
                 <p>
@@ -22,7 +22,7 @@ export default function Projects () {
                 <h2>CodeClan - Group</h2>
                 <div className="desImgFormat">
                 <p>descritption</p>
-                <img src="../src/imgs/codeClanDashBoard.png" alt=""  width="800" height="600"/>
+                <img src="../src/imgs/codeClanDashBoard.jpg" alt=""  width="800" height="600"/>
                 </div>
                 <div className="links">
                 <p>
@@ -35,7 +35,7 @@ export default function Projects () {
                 <h2>GamerCore - Solo</h2>
                 <div className="desImgFormat">
                 <p>Project description</p>
-                <img src="../src/imgs/GamerCoreFeed.png" alt=""  width="800" height="600"/>
+                <img src="../src/imgs/GamerCoreFeed.jpg" alt=""  width="800" height="600"/>
                 </div>
                 <div className="links">
                 <p>
