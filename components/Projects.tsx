045a7bf8ae1c68b266e8299cb97eae13397d7fb0
Asad Jawaid <asazycat@gmail.com>
2024-02-27 06:45:59 +0000

    responsiveness

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,7 +9,7 @@ export default function Projects () {
                 <h2>Norcthcoders News Network (NC News) - Solo</h2>
                 <div className="desImgFormat">
                 <p>Project description</p>
-                <img src="../images/articlesNCNews.jpg" alt="" width="800" height="600"/>
+                <img src="../images/articlesNCNews.jpg" alt="" className="projectPhoto"/>
                 </div>
                 <div className="links">
                 <p>
@@ -22,7 +22,7 @@ export default function Projects () {
                 <h2>CodeClan - Group</h2>
                 <div className="desImgFormat">
                 <p>descritption</p>
-                <img src="../images/CodeClanUsers.jpg" alt=""  width="800" height="600"/>
+                <img src="../images/CodeClanUsers.jpg" alt="" className="projectPhoto"/>
                 </div>
                 <div className="links">
                 <p>
@@ -35,7 +35,7 @@ export default function Projects () {
                 <h2>GamerCore - Solo</h2>
                 <div className="desImgFormat">
                 <p>Project description</p>
-                <img src="../images/GamerCoreFeed.jpg" alt=""  width="800" height="600"/>
+                <img src="../images/GamerCoreFeed.jpg" alt=""  className="projectPhoto"/>
                 </div>
                 <div className="links">
                 <p>
